76086460fd7e6d265389a212d11a69833c4e3511
niuniu315 <454378439@qq.com>
2022-07-06 15:27:56 +0800

    初始化项目，添加Login组件

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="login">
+  <h1>{{msg}}</h1>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Login',
+  data() {
+    return {
+      msg: '这是加载页面'
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.login{
+  color: red;
+}
+</style>
