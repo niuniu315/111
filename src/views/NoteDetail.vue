1800786d6a11492728861e82285b28c937bcaf73
niuniu315 <454378439@qq.com>
2022-11-29 22:52:09 +0800

    完成笔记详情页静态布局

--- src/views/NoteDetail.vue
+++ src/views/NoteDetail.vue
@@ -1,19 +1,33 @@
 <template>
   <div id="note" class="detail">
     <NoteSidebar></NoteSidebar>
-    <div id="note-detail">
-      <h1>notebookId: {{ $route.query.notebookId }}</h1>
-      <h1>noteId: {{ $route.query.noteId }}</h1>
+    <div class="note-detail">
+      <div class="note-bar">
+        <span> 创建日期: 2天前</span>
+        <span> 更新日期: 1分钟前</span>
+        <span> 已保存</span>
+        <Icon name="huishouzhan" class="iconfont"/>
+        <Icon name="yulan" class="iconfont"/>
+      </div>
+      <div class="note-title">
+        <input type="text" placeholder="输入标题">
+      </div>
+      <div class="editor">
+        <textarea v-show="true" placeholder="输入内容, 支持 markdown 语法"></textarea>
+        <div class="preview markdown-body" v-html="" v-show="false">
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import NoteSidebar from "../components/NoteSidebar.vue";
+import Icon from "@/components/Icon";
 
 export default {
   name: 'NoteDetail',
-  components: {NoteSidebar},
+  components: {NoteSidebar, Icon},
   data() {
     return {
       msg: '笔记详情页'
@@ -22,7 +36,9 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "../assets/style/notedetail.scss";
+
 .detail {
   display: flex;
   align-items: stretch;
