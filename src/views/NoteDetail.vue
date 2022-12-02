f9b24b14efa73c678488183f65bc0935f6af09c1
niuniu315 <454378439@qq.com>
2022-12-02 12:43:06 +0800

    使用路由导航守卫后出现bug----2

--- src/views/NoteDetail.vue
+++ src/views/NoteDetail.vue
@@ -3,18 +3,17 @@
     <NoteSidebar @update:notes="val => notes = val"></NoteSidebar>
     <div class="note-detail">
       <div class="note-bar">
-        {{curBook}}
-        <span> 创建日期: {{ curBook.createdAtFriendly }}</span>
-        <span> 更新日期: {{ curBook.updatedAtFriendly }}</span>
-        <span> {{ curBook.statusText }}</span>
+        <span> 创建日期: {{ curNote.createdAtFriendly }}</span>
+        <span> 更新日期: {{ curNote.updatedAtFriendly }}</span>
+        <span> {{ curNote.statusText }}</span>
         <Icon name="huishouzhan" class="iconfont"/>
         <Icon name="yulan" class="iconfont"/>
       </div>
       <div class="note-title">
-        <input type="text" v-model:value="curBook.title" placeholder="输入标题">
+        <input type="text" v-model:value="curNote.title" placeholder="输入标题">
       </div>
       <div class="editor">
-        <textarea v-show="true" v-model:value="curBook.content" placeholder="输入内容, 支持 markdown 语法"></textarea>
+        <textarea v-show="true" v-model:value="curNote.content" placeholder="输入内容, 支持 markdown 语法"></textarea>
         <div class="preview markdown-body" v-html="" v-show="false">
         </div>
       </div>
@@ -26,14 +25,13 @@
 import NoteSidebar from "../components/NoteSidebar.vue";
 import Icon from "@/components/Icon";
 import LoginApi from "@/api/LoginApi";
-import Vuee from '@/helper/vuee'
 
 export default {
   name: 'NoteDetail',
   components: {NoteSidebar, Icon},
   data() {
     return {
-      curBook: {},
+      curNote: {},
       notes: []
     }
   },
@@ -43,15 +41,12 @@ export default {
         this.$router.push({path: '/login'});
       }
     });
-    Vuee.$once('update:notes', val => {
-      this.curNote = val.find(note => note.id == this.$route.query.noteId) || {}
-    })
   },
-  beforeRouteUpdate (to, from, next) {
-    this.curBook = this.notes.find(notebook => notebook.id == to.query.notebookId) || {}
-    console.log(to,from)
+  beforeRouteUpdate(to, from, next) {
+    this.curNote = this.notes.find(note => note.id == to.query.noteId)
+    console.log(to, from)
     console.log(this.notes);
-    console.log(this.curBook)
+    console.log(to.query.noteId)
     next()
   }
 };
